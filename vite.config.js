af59c6e20d214c6412438251d5a942b0ee06712e
Griselda Balmaceda <griseldawork1234@gmail.com>
2024-04-12 16:58:33 -0500

    correct config

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // Output directory for the build
-    outDir: "dist",
-    // Specify public base path for assets
-    base: "/TicTacToe/",
-    // Enable sourcemap generation for debugging
-    sourcemap: true,
-    // Additional build options as needed
-  },
+  base: "./",
 })
