bbb7fef96530bf265bf826bf63e88e7092225b55
Griselda Balmaceda <griseldawork1234@gmail.com>
2024-04-12 16:49:17 -0500

    update packages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Output directory for the build
+    outDir: "dist",
+    // Specify public base path for assets
+    base: "/TicTacToe/",
+    // Enable sourcemap generation for debugging
+    sourcemap: true,
+    // Additional build options as needed
+  },
 })
